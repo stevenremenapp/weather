bbd0dd8a43b0cba780df91eb9c6b4564ab0e3fae
stevenremenapp <remenapp@gmail.com>
2018-04-16 22:35:08 -0400

    add API!

--- scripts.js
+++ scripts.js
@@ -1,12 +1,33 @@
-//Get latitude, longitude, accuracy, and timestamp
+//Check for browser geolocation abilities
 if (navigator.geolocation) {
+  //Get latitude, longitude, accuracy, and timestamp
   navigator.geolocation.getCurrentPosition(function(position) {
-    var loc = document.querySelector('#data');
-    var acc = document.querySelector('#accuracy');
-    var ts = document.querySelector('#timestamp');
-    loc.innerHTML = "Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude;
-    acc.innerHTML = "Accuracy: " + position.coords.accuracy + " m";
-    var timeStamp = new Date(position.timestamp);
-    ts.innerHTML = "Time: " + timeStamp.toLocaleString();
-  })
-}
+    let latText = document.querySelector('#latitude');
+    let lonText = document.querySelector('#longitude');
+    let acc = document.querySelector('#accuracy');
+    let ts = document.querySelector('#timestamp');
+    let latitudeMeasurement = position.coords.latitude;
+    let longitudeMeasurement = position.coords.longitude;
+
+    latText.textContent = "Latitude: " + latitudeMeasurement;
+    lonText.textContent = "Longitude: " + longitudeMeasurement;
+    acc.textContent = "Accuracy: " + position.coords.accuracy + " m";
+    let timeStamp = new Date(position.timestamp);
+    ts.textContent = "Time: " + timeStamp.toLocaleString();
+
+    //make API call for weather data
+    let request = new XMLHttpRequest();
+    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitudeMeasurement}&lon=${longitudeMeasurement}&units=imperial&appid=b056e3da825f49292fe0d4b5f8f8d9b3`;
+
+    request.onreadystatechange = function() {
+      if (this.readyState === 4 && this.status === 200) {
+        //console.log(request.responseText);
+        console.log(request.main.temp);
+      }
+    }
+
+    request.open("GET", url, true);
+    request.send();
+
+  });
+};
