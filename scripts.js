8bf77b02fe53252b7a2814df18f7a1ddc1a3b52d
stevenremenapp <remenapp@gmail.com>
2018-04-21 16:57:04 -0400

    Add ability to change background gradients based on weather conditions and night/day

--- scripts.js
+++ scripts.js
@@ -43,6 +43,83 @@ if (navigator.geolocation) {
         iconDisplay.style.width = '100px';
         //iconDisplay.src = iconUrl;
 
+        //change the background gradient based on response.weather[0].main
+        let weather = response.weather[0].main;
+        console.log(weather);
+
+        //if it is nighttime add an additional gradient to darken the screen
+        //Use the JavaScript Date Object to get the current time of day and convert it to Unix timestamp. JS gets time in ms, so convert to secs for Unix
+        let unixTimeStamp = Math.floor(Date.now() / 1000);
+        console.log(response.sys.sunrise);
+        console.log(unixTimeStamp);
+        console.log(response.sys.sunset);
+
+
+        switch (weather) {
+          case 'Thunderstorm':
+            if (unixTimeStamp > response.sys.sunrise && unixTimeStamp < response.sys.sunset) {
+              document.body.style.background = "linear-gradient(to right, #283048, #859398)";
+            } else {
+              document.body.style.background = "linear-gradient(to right, rgba(84, 84, 84, 0.7), rgba(84, 84, 84, 0.7)), linear-gradient(to right, #283048, #859398)";
+            }
+            break;
+          case 'Drizzle':
+            if (unixTimeStamp > response.sys.sunrise && unixTimeStamp < response.sys.sunset) {
+              document.body.style.background = "linear-gradient(to right, #4ca1af, #c4e0e5)";
+            } else {
+              document.body.style.background = "linear-gradient(to right, rgba(84, 84, 84, 0.7), rgba(84, 84, 84, 0.7)), linear-gradient(to right, #4ca1af, #c4e0e5)";
+            }
+            break;
+          case 'Rain':
+            if (unixTimeStamp > response.sys.sunrise && unixTimeStamp < response.sys.sunset) {
+              document.body.style.background = "linear-gradient(to right, #667db6, #0082c8, #0082c8, #667db6)";
+            } else {
+              document.body.style.background = "linear-gradient(to right, rgba(84, 84, 84, 0.7), rgba(84, 84, 84, 0.7)), linear-gradient(to right, #667db6, #0082c8, #0082c8, #667db6)";
+            }
+            break;
+          case 'Snow':
+            if (unixTimeStamp > response.sys.sunrise && unixTimeStamp < response.sys.sunset) {
+              document.body.style.background = "linear-gradient(to right, #e6dada, #274046)";
+            } else {
+              document.body.style.background = "linear-gradient(to right, rgba(84, 84, 84, 0.7), rgba(84, 84, 84, 0.7)), linear-gradient(to right, #e6dada, #274046)";
+            }
+            break;
+          case 'Atmosphere':
+            if (unixTimeStamp > response.sys.sunrise && unixTimeStamp < response.sys.sunset) {
+              document.body.style.background = "linear-gradient(to right, #3e5151, #decba4)";
+            } else {
+              document.body.style.background = "linear-gradient(to right, rgba(84, 84, 84, 0.7), rgba(84, 84, 84, 0.7)), linear-gradient(to right, #3e5151, #decba4)";
+            }
+            break;
+          case 'Clear':
+            if (unixTimeStamp > response.sys.sunrise && unixTimeStamp < response.sys.sunset) {
+              document.body.style.background = "linear-gradient(to right, #1c92d2, #f2fcfe)";
+            } else {
+              document.body.style.background = "linear-gradient(to right, rgba(84, 84, 84, 0.7), rgba(84, 84, 84, 0.7)), linear-gradient(to right, #1c92d2, #f2fcfe)";
+            }
+            break;
+          case 'Clouds':
+            if (unixTimeStamp > response.sys.sunrise && unixTimeStamp < response.sys.sunset) {
+              document.body.style.background = "linear-gradient(to right, #e2dfdc, #ffffff)";
+            } else {
+              document.body.style.background = "linear-gradient(to right, rgba(84, 84, 84, 0.7), rgba(84, 84, 84, 0.7)), linear-gradient(to right, #e2dfdc, #ffffff)";
+            }
+            break;
+          case 'Extreme':
+            if (unixTimeStamp > response.sys.sunrise && unixTimeStamp < response.sys.sunset) {
+              document.body.style.background = "linear-gradient(to right, #40e0d0, #ff8c00, #ff0080)";
+            } else {
+              document.body.style.background = "linear-gradient(to right, rgba(84, 84, 84, 0.7), rgba(84, 84, 84, 0.7)), linear-gradient(to right, #40e0d0, #ff8c00, #ff0080)";
+            }
+            break;
+          default:
+            if (unixTimeStamp > response.sys.sunrise && unixTimeStamp < response.sys.sunset) {
+              document.body.style.background = "linear-gradient(to right, #E9E4F0, #D3CCE3)";
+            } else {
+              document.body.style.background = "linear-gradient(to right, rgba(84, 84, 84, 0.7), rgba(84, 84, 84, 0.7)), linear-gradient(to right, #E9E4F0, #D3CCE3)";
+            }
+        }
+
         //display the location
         let location = document.querySelector('#location');
         location.textContent = "Location: " + response.name + ",";
